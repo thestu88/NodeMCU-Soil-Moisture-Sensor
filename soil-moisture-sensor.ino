7aedc8ed055c7be58dd229cdcf124848a7dbeebd
Stuart McDaniel <mcdanielsl88@gmail.com>
2022-10-21 13:14:21 -0400

    Fixed a typo
    Fixed a typo

--- soil-moisture-sensor.ino
+++ soil-moisture-sensor.ino
@@ -1,3 +1,15 @@
+/*
+  Rui Santos
+  Complete project details at Complete project details at https://RandomNerdTutorials.com/esp8266-nodemcu-http-get-post-arduino/
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files.
+  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+  
+  Code compatible with ESP8266 B
+  oards Version 3.0.0 or above 
+  (see in Tools > Boards > Boards Manager > ESP8266)
+*/
+
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 #include <WiFiClient.h>
@@ -6,12 +18,12 @@ const char* ssid = "YOUR_WIFI_SSID";
 const char* password = "YOUR_WIFI_PASSWORD";
 
 #define sensorId  1 //identifier
-#define sensorLoc  "weird sensor //identifier
-"
+#define sensorLoc  "weird sensor" //identifier
+
 #define analogPin A0
 int adcValue = 0;
 
-const char* serverName = "YOUR_SERVER_URL_ENDPOINT";  //Might look something like http://rpi.local:5001/Home/PostTemp if you set up the project on a raspberry pi similarly
+const char* serverName = "http://rpi.local:5001/Home/PostTemp";
 
 unsigned long lastTime = 0;
 unsigned long timerDelay = 5000;
@@ -25,7 +37,11 @@ void setup() {
     delay(500);
     Serial.print(".");
   }
-  Serial.print("Connected to WiFi!");
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
+ 
+  Serial.println("Timer set to 5 seconds (timerDelay variable), it will take 5 seconds before publishing the first reading.");
 }
 
 void loop() {
@@ -49,7 +65,7 @@ void loop() {
       Serial.println(httpResponseCode);
       http.end();
 
-      delay(600000); //10min
+      delay(600000);
     }
     else {
       Serial.println("WiFi Disconnected");
@@ -59,7 +75,9 @@ void loop() {
         delay(500);
         Serial.print(".");
       }
-      Serial.print("Connected to WiFi!");
+      Serial.println("");
+      Serial.print("Connected to WiFi network with IP Address: ");
+      Serial.println(WiFi.localIP());
     }
     lastTime = millis();
   }
